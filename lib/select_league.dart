fdff0b7846e087e98f58116ff00cb41d3cc8f347
Ingmar <struijs@gmail.com>
2020-09-01 21:26:00 +0200

    formatting update

--- lib/select_league.dart
+++ lib/select_league.dart
@@ -8,28 +8,27 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'models/league.dart';
 
 class SelectLeague extends StatefulWidget {
-
   @override
   _SelectLeagueState createState() => _SelectLeagueState();
 }
 
 class _SelectLeagueState extends State<SelectLeague> {
-
-  final String _pouleIdsUrl = 'https://cors-anywhere.herokuapp.com/http://cm.nzvb.nl/modules/nzvb/api/poule_ids.php';
+  final String _pouleIdsUrl =
+      'https://cors-anywhere.herokuapp.com/http://cm.nzvb.nl/modules/nzvb/api/poule_ids.php';
   List<League> _leagues = new List<League>();
   League _selectedLeague = new League('', '');
 
   Future<List<League>> getLeagues() async {
-    var res = await http.get(Uri.encodeFull(_pouleIdsUrl), headers: {'X-Requested-With': 'XMLHttpRequest'});
+    var res = await http.get(Uri.encodeFull(_pouleIdsUrl),
+        headers: {'X-Requested-With': 'XMLHttpRequest'});
     Map resBody = jsonDecode(res.body);
     var test = resBody.keys.toList()..sort();
     _leagues.clear();
 
-    //TODO pak de laatste seizoen die actief is
+    // ACTIVE COMPETITION ID 2020/2021 = 6
     Map<String, String> values = Map<String, String>.from(resBody[test[4]]);
     values.forEach((k, v) => _leagues.add(League(k, v)));
-    // hebben we hier niet nodig
-    _leagues.removeWhere((x)=> x.name == "Cup poule");
+    _leagues.removeWhere((x) => x.name == "Cup poule");
     return _leagues;
   }
 
@@ -48,34 +47,36 @@ class _SelectLeagueState extends State<SelectLeague> {
           centerTitle: true,
         ),
         body: new Column(
-        children: <Widget>[
-          new Container( height: 20,),
-          new Expanded(
-            child: FutureBuilder(
+          children: <Widget>[
+            new Container(
+              height: 20,
+            ),
+            new Expanded(
+                child: FutureBuilder(
               future: getLeagues(),
-              builder: (BuildContext context, AsyncSnapshot<List<League>> snapshot) {
-              if (!snapshot.hasData) return Container();              
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final data = snapshot.data[index];
-                  return ListTile(
-                    title: Text(data.name),
-                    trailing: Icon(Icons.chevron_right),
-                    onTap: () {
-                      setState(() {
-                        _selectedLeague = data;
-                      });
-                      saveLeague();
-                      Navigator.pop(context, _selectedLeague.name);
-                    },
-                  );
-                },
-              );
+              builder:
+                  (BuildContext context, AsyncSnapshot<List<League>> snapshot) {
+                if (!snapshot.hasData) return Container();
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final data = snapshot.data[index];
+                    return ListTile(
+                      title: Text(data.name),
+                      trailing: Icon(Icons.chevron_right),
+                      onTap: () {
+                        setState(() {
+                          _selectedLeague = data;
+                        });
+                        saveLeague();
+                        Navigator.pop(context, _selectedLeague.name);
+                      },
+                    );
+                  },
+                );
               },
             ))
-        ],
-      )
-    );
+          ],
+        ));
   }
 }
